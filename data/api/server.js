4b879591768e5e8358dd10dcc88edef52faeec5d
Katrina_Dierking <kalynndier@gmail.com>
2020-01-22 13:08:11 -0500

    index and server created

---
+++ data/api/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const server = express();
+
+const postsRouter = require('../posts/posts-router');
+
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+    res.send({ Hello: "Taran!!! :-) " });
+  });
+
+  server.use('/api/posts', postsRouter)
+
+  module.exports = server;
---
+++ index.js
@@ -0,0 +1,6 @@
+const server = require('./data/api/server.js');
+
+
+server.listen(6000, () => {
+    console.log('\n *** Server Running on http://localhost:6000 ***');
+});
